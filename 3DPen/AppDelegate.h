632f0ed327ba046e4738e64516fdbd9f2e0302b7
Warren Whipple <warrenwhipple@Warrens-MacBook-Air.local>
2012-03-17 17:43:24 -0400

    Initial Commit

---
+++ 3DPen/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  3DPen
+//
+//  Created by Warren Whipple on 3/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 3DPen/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  3DPen
+//
+//  Created by Warren Whipple on 3/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
